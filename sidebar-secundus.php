e0aa276fc6ed9f1472b26c60545f67782600d913
tregenza <junk-github@tregenza.com>
2018-05-01 17:20:09 +0100

    Work on sidebars

--- sidebar-secundus.php
+++ sidebar-secundus.php
@@ -2,11 +2,15 @@
 	<aside class="secundus">
 	<?php
 
+		function noMenuFallback() {
+			echo "<h4>No Menu Defined</h4><p>Please set the Navigation Logged In and Navigation Logged Out menu locations";			
+		}
+
 		echo '<div class="tregenzaOneAside secundusSidebar hamburgerMenuWrapperSidebar" >';
 			if ( is_user_logged_in() ) {
-				wp_nav_menu( array( 'theme_location' => 'hamburgerLoggedIn', 'container_id' => 'hamburgerMenu' , 'container_class' => 'hamburgerMenucontainer' ) ); 
+				wp_nav_menu( array( 'theme_location' => 'hamburgerLoggedIn', 'container_id' => 'hamburgerMenu' , 'container_class' => 'hamburgerMenucontainer', 'fallback_cb' => 'noMenuFallback' ) ); 
 			} else {
-				wp_nav_menu( array( 'theme_location' => 'hamburger', 'container_id' => 'hamburgerMenu' , 'container_class' => 'hamburgerMenucontainer' ) ); 
+				wp_nav_menu( array( 'theme_location' => 'hamburger', 'container_id' => 'hamburgerMenu' , 'container_class' => 'hamburgerMenucontainer', 'fallback_cb' => 'noMenuFallback' ) ); 
 			}
 		echo '</div>';
 
